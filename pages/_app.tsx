494e4e95e67a72840b4950bb0b6adb52c3feb03c
Jonas Wolfram <jonas-wolfram@web.de>
2022-12-03 11:43:10 +0530

    Implemented wallet connection

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,21 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import '../styles/globals.css';
+import type { AppProps } from 'next/app';
+import { WagmiConfig, createClient } from 'wagmi';
+import { ConnectKitProvider, getDefaultClient } from 'connectkit';
+
+const client = createClient(
+  getDefaultClient({
+    appName: 'Your App Name',
+    autoConnect: true,
+  })
+);
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <WagmiConfig client={client}>
+      <ConnectKitProvider>
+        <Component {...pageProps} />;
+      </ConnectKitProvider>
+    </WagmiConfig>
+  );
 }
